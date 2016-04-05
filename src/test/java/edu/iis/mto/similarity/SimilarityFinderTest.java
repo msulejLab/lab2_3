3daf20fa8495cb1a0657033e581baddd2a810e7a
Maciej Sulej <187809@edu.p.lodz.pl>
2016-04-05 14:05:12 +0200

    Dodanie emptySequencesShouldReturnOne

---
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -0,0 +1,27 @@
+package edu.iis.mto.similarity;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.closeTo;
+
+import static org.junit.Assert.*;
+
+public class SimilarityFinderTest {
+    private static final double DELTA = 0.0001;
+
+    private SequenceSearcherStub searcher;
+    private SimilarityFinder similarityFinder;
+
+    int[] seq1, seq2;
+
+    @Test
+    public void emptySequencesShouldReturnOne() {
+        searcher = new SequenceSearcherStub();
+        similarityFinder = new SimilarityFinder(searcher);
+        seq1 = new int[] {};
+        seq2 = new int[] {};
+
+        double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+        assertThat(result, closeTo(1, DELTA));
+    }
+}
