749be7b541317b96c0c4244fda909a78f5321564
Maciej Sulej <187809@edu.p.lodz.pl>
2016-04-05 14:29:35 +0200

    Dodanie specificSequencesTest

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -7,7 +7,7 @@ import static org.hamcrest.Matchers.closeTo;
 import static org.junit.Assert.*;
 
 public class SimilarityFinderTest {
-    private static final double DELTA = 0.0001;
+    private static final double DELTA = 0.001;
 
     private SequenceSearcherStub searcher;
     private SimilarityFinder similarityFinder;
@@ -45,5 +45,16 @@ public class SimilarityFinderTest {
 
         double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
         assertThat(result, closeTo(0, DELTA));
+    }
+
+    @Test
+    public void specificSequencesTest() {
+        searcher = new SequenceSearcherStub();
+        similarityFinder = new SimilarityFinder(searcher);
+        seq1 = new int[] {7, 3, 2, 4, 1};
+        seq2 = new int[] {4, 1, 9, 7, 5};
+
+        double result = similarityFinder.calculateJackardSimilarity(seq1, seq2);
+        assertThat(result, closeTo(0.429, DELTA));
     }
 }
